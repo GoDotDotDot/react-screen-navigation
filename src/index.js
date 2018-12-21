1af30abd0149c07cd88a872850cc6dfc4a4a91b6
GoDotDotDot <chukui0202@gmail.com>
2018-12-22 01:17:56 +0800

    build gh-pages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Screen from './Screen';
-import ScreenProvider from './ScreenProvider';
-import withScreen from './withScreen';
-
-const ReactScreenNavigaton = { Screen, ScreenProvider, withScreen };
-
-export default ReactScreenNavigaton;
-export { Screen, ScreenProvider, withScreen };
